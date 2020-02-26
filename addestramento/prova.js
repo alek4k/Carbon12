597ec391ed8ed4e8731bcd3633b6463d87f6e33b
pederivaveronica <veronicapederiva@MacBook-Pro-di-Veronica.local>
2020-02-26 16:57:00 +0100

    aggiunto controllo per blocco svm, riordinato file prova, aggiunto link homepage su download predittore

--- addestramento/prova.js
+++ addestramento/prova.js
@@ -93,57 +93,66 @@ function addestramento(data, labels){
 //funzione per gestire dati in input nella form
 function uploadForm(req, res, form){
   form.parse(req, function (err, fields, files) {
-      model = fields.modello;
+    model = fields.modello;
+    
+    //file csv addestramento
+    // oldpath : dir temporanea dove è salvato il file
+    var oldpathTrainFile = files.trainFile.path;
+    // newpath : nuova dir dove è salvato il file
+    var newpathTrainFile = upload_path + files.trainFile.name;
+    // copia del file nella nuova posizione
+    fs.rename(oldpathTrainFile, newpathTrainFile, (err) => {
+      //if (err) throw err;
+      //controllo validità file --> popup errore
+      console.log('Rename complete!');
+    });
 
-      //file csv addestramento
-      // oldpath : dir temporanea dove è salvato il file
-      var oldpathTrainFile = files.trainFile.path;
-      // newpath : nuova dir dove è salvato il file
-      var newpathTrainFile = upload_path + files.trainFile.name;
-      // copia del file nella nuova posizione
-      fs.rename(oldpathTrainFile, newpathTrainFile, (err) => {
-        if (err) throw err;
-        console.log('Rename complete!');
-      });
+    //file json config
+    // oldpath : dir temporanea dove è salvato il file
+    var oldpathConfigFile = files.configFile.path;
+    // newpath : nuova dir dove è salvato il file
+    var newpathConfigFile = upload_path + files.configFile.name;
+    // copia del file nella nuova posizione
+    fs.rename(oldpathConfigFile, newpathConfigFile, (err) => {
+      //if (err) throw err;
+      //controllo validità file --> popup errore
+      console.log('Rename complete!');
+    });
 
-      //file json config
-      // oldpath : dir temporanea dove è salvato il file
-      var oldpathConfigFile = files.configFile.path;
-      // newpath : nuova dir dove è salvato il file
-      var newpathConfigFile = upload_path + files.configFile.name;
-      // copia del file nella nuova posizione
-      fs.rename(oldpathConfigFile, newpathConfigFile, (err) => {
-        if (err) throw err;
-        console.log('Rename complete!');
-      });
+    //lettura dati per addestramento: data e labels
+    var datainput=fs.readFileSync(newpathConfigFile, 'utf8');
+    var obj= JSON.parse(datainput);
+    var datagraf=[];
+    var labels=[];
+    var i=0;
+    for(var key in obj){
+      datagraf[i]=key;
+      labels[i]=obj[key];
+      i=i+1;
+    }
+    var data=[];
+    for(let a=0; a<datagraf.length; a++){
+      data[a] = [];
+      for(var b=0; b<2; b++) {
+        data[a][b] = readNumero(datagraf[a],b);
+      }
+    }
 
-          //if (err) throw err; //deve lanciare errore se controllo file non va a buon fine: ALERT Popup?
-         //lettura dati per addestramento: data e labels
-          var datainput=fs.readFileSync(newpathConfigFile, 'utf8');
-          var obj= JSON.parse(datainput);
-          var datagraf=[];
-          var labels=[];
-          var i=0;
-          for(var key in obj){
-            datagraf[i]=key;
-            labels[i]=obj[key];
-            i=i+1;
-          }
-          var data=[];
-          for(let a=0; a<datagraf.length; a++){
-            data[a] = [];
-            for(var b=0; b<2; b++) {
-              data[a][b] = readNumero(datagraf[a],b);
-            }
-          }
-
-          //analizzare select SVM o RL
-          var risultato= addestramento(data,labels);
-          //redirect alla pagina di download
-          console.log("addestramento terminato");
-          fs.writeFileSync('predittore.json',JSON.stringify(risultato));
-          res.writeHead(301,{'Location' : 'downloadPredittore'});
-          return res.end();
+    if(model == 'SVM'){
+      //chiamata function addestramento SVM
+      console.log("support");
+    }
+    else {
+      //chiamata function addestramento RL
+      console.log("regression");
+    }
+    //analizzare select SVM o RL
+    var risultato= addestramento(data,labels);
+    //redirect alla pagina di download
+    console.log("addestramento terminato");
+    fs.writeFileSync('predittore.json',JSON.stringify(risultato));
+    res.writeHead(301,{'Location' : 'downloadPredittore'});
+    return res.end();
   });
 }
 
@@ -159,4 +168,5 @@ function downloadPredittore(req, res){
   var filestream = fs.createReadStream(file);
   filestream.pipe(res);
   //aggiungere conferma download e redirect a upload
+
 }

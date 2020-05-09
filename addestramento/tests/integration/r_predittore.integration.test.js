04fd175bebb3f12a928d3a216176b24869225f7f
Francesco G <56235596+FrancescoGobbo@users.noreply.github.com>
2020-05-09 09:51:22 +0200

    Update integration test per rpredittore

--- addestramento/tests/integration/r_predittore.integration.test.js
+++ addestramento/tests/integration/r_predittore.integration.test.js
@@ -23,3 +23,53 @@ test('It should response that Plugin\'s and train\'s version are compatible', ()
 test('It should response that Plugin\'s and train\'s version are compatible', () => {
     expect(managePredittore.validity()).toBeTruthy();
 });
+
+test('It should response that file.json has the expected title', () => {
+    expect(managePredittore.getTitle()).toEqual('Carbon12 Predire in Grafana');
+});
+
+test('It should response that file.json has the Plugin expected version', () => {
+    expect(managePredittore.getPluginVersion()).toEqual('0.0.0');
+});
+
+test('It should response that file.json has the Train expected version', () => {
+    expect(managePredittore.getTrainVersion()).toEqual('0.0.0');
+});
+
+test('It should response that file.json has the expected sources', () => {
+    expect(managePredittore.getDataEntry()).toEqual(['A', 'B']);
+});
+
+test('It should response that file.json has no data_entry', () => {
+    expect(managePredittore.getDataEntry()).toEqual(['A', 'B']);
+});
+
+test('It should response that file.json has the expected model', () => {
+    expect(managePredittore.getModel()).toEqual('SVM');
+});
+
+test('It should response that file.json has no File version', () => {
+    expect(managePredittore.getFileVersion()).toBe(0);
+});
+
+test('It should response that file.json has File version', () => {
+    expect(managePredittore.getFileVersion()).toBe(0);
+});
+
+test('It should response that file.json has the expected notes', () => {
+    expect(managePredittore.getNotes()).toEqual('test');
+});
+
+test('It should response that file.json has the expected configuration', () => {
+    const k = {};
+    k.N = 60;
+    k.D = 3;
+    k.b = 25889865728;
+    k.kernelType = 'linear';
+    k.w = [
+        -4838.896484375,
+        489.25675450149345,
+        1224.422537904899,
+    ];
+    expect(managePredittore.getConfiguration()).toEqual(k);
+});
